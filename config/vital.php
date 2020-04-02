73c30d6b629a702e4d532733f8d0b056b8667bcf
shinpei_yoshihara <s-yoshihara@saitama-tec.co.jp>
2020-04-02 11:43:44 +0900

    死活監視機能追加

---
+++ config/vital.php
@@ -0,0 +1,5 @@
+<?php
+return [
+  //死活監視送信先のURLを設定する
+  'url' => env('VITAL_SIGNAL_URL', 'http://127.0.0.1/')
+];
--- routes/console.php
+++ routes/console.php
@@ -2,18 +2,11 @@
 
 use Illuminate\Foundation\Inspiring;
 use Illuminate\Support\Facades\Artisan;
+use Illuminate\Support\Facades\Http;
 
-/*
-|--------------------------------------------------------------------------
-| Console Routes
-|--------------------------------------------------------------------------
-|
-| This file is where you may define all of your Closure based console
-| commands. Each Closure is bound to a command instance allowing a
-| simple approach to interacting with each command's IO methods.
-|
-*/
-
-Artisan::command('inspire', function () {
-    $this->comment(Inspiring::quote());
-})->describe('Display an inspiring quote');
+Artisan::command('vital', function () {
+  $response=Http::get(config("vital.url"));
+  \Log::info("vital");
+  echo($response->status());
+  echo($response->ok());
+})->describe('vital signal send');
